05b1443d7f74d14c4444f333bc934f03c4e23f66
abarker-byui <bar17115@byui.edu>
2019-05-18 12:42:55 -0700

    Added hello response from Andrew

--- src/Nielson/HelloThere.java
+++ src/Nielson/HelloThere.java
@@ -4,6 +4,7 @@ public class HelloThere {
 
     public static void main(String[] args){
         System.out.println("Hello, my name is Thomas.");
+		System.out.println("Hello Thomas, my name is Andrew.");
     }
 
 }

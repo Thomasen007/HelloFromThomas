7e683fbbf7cb5d4fd9c4dadb0cfbc114881169ca
thomasen007 <thomas.elliott.nielson@gmail.com>
2019-05-18 10:11:57 -0700

    updates

--- src/Nielson/HelloThere.java
+++ src/Nielson/HelloThere.java
@@ -1,9 +1,18 @@
 package Nielson;
 
+import java.util.Scanner;
+
 public class HelloThere {
 
     public static void main(String[] args){
-        System.out.println("Hello, my name is Thomas.");
+
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Hello, my name is Thomas. What is your name? ");
+        String name = myObj.nextLine();
+        System.out.println("Hello " + name +" nice to meet you.");
     }
+
+
 
 }
